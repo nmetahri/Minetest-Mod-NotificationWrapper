11da209f378a1fa22f9bec8d2160bd189a8079c1
Thomas OBEIN <kirowl@proton.me>
2024-10-08 21:55:23 +0200

    Change notification position (center -> top right) + add title parameter

--- notifications_wrapper/init.lua
+++ notifications_wrapper/init.lua
@@ -3,13 +3,14 @@ local active_notifications = {}
 local hudElements = {}
 local pending_notifications = {}
 
-function notifications.create(playerName, text, image, timeout)
+function notifications.create(playerName, title, text, image, timeout)
     if playerName == "" then
         return
     end
 
     local notification = {
         playerName = playerName,
+        title = title or "Notification",
         text = text or "",
         image = image or "default-bg.jpg",
         timeout = timeout or 10
@@ -25,6 +26,14 @@ function notifications.does_image_exists(image_name)
     return image ~= nil
 end
 
+local function split_text_into_lines(text, max_length)
+    local lines = {}
+    for i = 1, #text, max_length do
+        table.insert(lines, text:sub(i, i + max_length - 1))
+    end
+    return lines
+end
+
 function notifications.show(notification)
     local player = minetest.get_player_by_name(notification.playerName)
     if not player then return end
@@ -33,26 +42,48 @@ function notifications.show(notification)
 
     table.insert(hudElements[notification.playerName], player:hud_add({
         hud_elem_type = "image",
-        position = { x = 0.5, y = 0.5 },
-        offset = { x = 0, y = -20 },
+        position = { x = 0.83, y = 0.1 },
+        offset = { x = 0, y = 0 },
         text = notifications.does_image_exists(notification.image) and notification.image or "default_bg.png",
-        scale = { x = 1, y = 1 },
+        scale = { x = 0.75, y = 0.75 },
         alignment = { x = 0, y = 0 },
     }))
 
     table.insert(hudElements[notification.playerName], player:hud_add({
         hud_elem_type = "text",
-        position      = { x = 0.5, y = 0.5 },
-        offset        = { x = 0, y = 20 },
-        text          = notification.text,
+        position      = { x = 0.83, y = 0.06 }, 
+        offset        = { x = 0, y = 0 },
+        text          = notification.title,
         alignment     = { x = 0, y = 0 },
         scale         = { x = 100, y = 100 },
         number        = 0xFFFFFF
     }))
 
+    table.insert(hudElements[notification.playerName], player:hud_add({
+        hud_elem_type = "text",
+        position      = { x = 0.83, y = 0.068 }, 
+        offset        = { x = 0, y = 0 },
+        text          = string.rep("-", #notification.title),
+        alignment     = { x = 0, y = 0 },
+        scale         = { x = 100, y = 100 },
+        number        = 0xFFFFFF
+    }))
+
+    local lines = split_text_into_lines(notification.text, 75) 
+    for i, line in ipairs(lines) do
+        table.insert(hudElements[notification.playerName], player:hud_add({
+            hud_elem_type = "text",
+            position      = { x = 0.83, y = 0.085 + (i - 1) * 0.02 }, 
+            offset        = { x = 0, y = 0 },
+            text          = line,
+            alignment     = { x = 0, y = 0 },
+            scale         = { x = 100, y = 100 },
+            number        = 0xFFFFFF
+        }))
+    end
+
     minetest.after(notification.timeout, function()
         notifications.clear_hud_elements(notification.playerName)
-        notifications.remove(notification)
     end)
 end
 
@@ -75,10 +106,10 @@ function notifications.remove(notification)
     end
 end
 
-function notifications.queue(playerName, text, image, timeout)
-    local notification = notifications.create(playerName, text, image, timeout)
+function notifications.queue(notification_params)
+    local notification = notifications.create(notification_params.playerName, notification_params.title, notification_params.text, notification_params.image, notification_params.timeout)
     table.insert(pending_notifications, notification)
-    notifications.process_queue(playerName)
+    notifications.process_queue(notification.playerName)
 end
 
 function notifications.process_queue(playerName)
@@ -99,7 +130,15 @@ minetest.register_chatcommand("notify", {
     params = "<message>",
     description = "Teste une notification",
     func = function(name, param)
-        notifications.queue(name, param, "dfs.jpg", 10)
+        notifications.queue(
+            {
+                playerName = name,
+                title = "This is the title",
+                text = param,
+                image = "dfs.jpg",
+                timeout = 10
+            }
+        )
     end
 })
 
